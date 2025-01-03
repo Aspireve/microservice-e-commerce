7d9a27aa1dcf17d5acfe8521c2d12bb0b9c2e795
Aspireve <steve18fernandes@gmail.com>
2025-01-03 19:32:05 +0530

    chore: added port

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -19,7 +19,7 @@ app.use(unknownEndpoints);
 // @ts-ignore
 app.use(errorHandler);
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT || 3000, () => {
   console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 });
 
